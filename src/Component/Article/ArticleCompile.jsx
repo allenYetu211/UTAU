69ee832fc22a82c0d99e792a0dc725c2152615c6
allen <allenwill211@gmail.com>
2017-11-07 00:39:58 +0800

    featL ArticleCompile.jsx -  add Compile

--- src/Component/Article/ArticleCompile.jsx
+++ src/Component/Article/ArticleCompile.jsx
@@ -1,22 +1,32 @@
 import React, {Component} from 'react';
-import axios from '../../Public/js/axios'
+import {Editor, EditorState, convertToRaw} from 'draft-js';
+
 class ArticleCompile extends Component {
-    // constructor(props) {
-    //     super(props)
-    // }
-    async componentWillMount () {
-        console.log(this.props.match.params.id)
-        const data = await axios.ajax('article/articleCompile',{
-            id: this.props.match.params.id
-        }, 'post')
-        console.log(data)
+    constructor(props) {
+        super(props);
+        this.state = {editorState: EditorState.createEmpty()};
+        this.onChange = (editorState) => this.setState({editorState});
     }
+
+    upDataArticle() {
+        const blocks = convertToRaw(this.state.editorState.getCurrentContent())
+        console.log(blocks)
+    }
+    // TODO 拼接JSON发送数据 - 存入数据库
+    // TODO 解析数据内容
     render() {
-        return(
-            <div>
-                tes
+        return (
+            <div className="article__content--compile">
+                <div className="article__content--title">
+                    <input type="text" placeholder="Title"/>
+                </div>
+
+                <div className="article__content--compile-main">
+                        <Editor editorState={this.state.editorState} onChange={this.onChange}/>
+                    </div>
+                    <button onClick={(e) => this.upDataArticle(e)}>updata</button>
             </div>
-        )
+        );
     }
 }
 
